ac7668ef851e0df348ab9f06822d0b7f38edfe93
Lee-Chan-bin <cksqls0104@gmail.com>
2025-02-05 17:12:25 +0900

    영역 추가

--- capture.py
+++ capture.py
@@ -19,8 +19,8 @@ def generate_frame():
     region = solutions.RegionCounter(
         show=True,
         region=region_points,
-        model='model_train_1/trained_yolo11n.pt'                                # 맥용 경로
-        # model = 'Pyeongtaek_AI_YOLO_Team/model_train_1/trained_yolo11n.pt     # 윈도우용 경로
+       # model='model_train_1/trained_yolo11n.pt'                                # 맥용 경로
+        model = 'Pyeongtaek_AI_YOLO_Team/model_train_1/trained_yolo11n.pt'     # 윈도우용 경로
     )
     
     
