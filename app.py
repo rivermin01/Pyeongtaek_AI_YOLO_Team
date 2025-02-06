9f43f50c35afcfe7f63cb1a7f59960548e3bf5a3
rivermin01 <ckm8870@naver.com>
2025-02-06 10:16:57 +0900

    불필요한 코드 삭제 및 주석 추가

--- app.py
+++ app.py
@@ -9,42 +9,54 @@ def video_feed():
     # 비디오 스트리밍 제공
     return Response(generate_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+'''
+./templates/
+
+blue.html:          파란 배경, 통행 가능
+yellow.html:        노란 배경, 주의하며 통행 가능,                   1차 경고음(static/Ysound.mp3)
+red.html:           빨간 배경, 통행 불가능,                        2차 경고음(static/Rsound.mp3)
+---------------------------------------------------------------------------
+yellow3.html(중앙용): 노란 배경, 2명, 1명으로 분할하여 통행,            안내 음성(static/y3.mp3)
+yellow4.html(중앙용): 노란 배경, 2명, 2명으로 분할하여 통행,            안내 음성(static/y4.mp3)
+red.html(중앙용):     빨간 배경, 2명씩 분할하여 통행, 남은 사람은 대기,    안내 음성(static/r5.mp3)
+'''
+
 # 1번 길 상황
 @app.route('/road1')
 def home1():
     while True:
-        status = get_status1()
-        if status <= 1:
+        status = get_status1()          # 좌측 계단에 있는 사람의 수에 따라 각각 다른 HTML파일 불러오기
+        if status <= 1:     # 1인 이하
             return render_template('blue.html')
-        elif status == 2:
+        elif status == 2:   # 1인 이하
             return render_template('yellow.html')
-        else:
+        else:               # 3인 이상
             return render_template('red.html')
 
 # 2번 길 상황
 @app.route('/road2')
 def home2():
     while True:
-        status = get_status2()
-        if status <= 1:
+        status = get_status2()          # 우측 계단에 있는 사람의 수에 따라 각각 다른 HTML파일 불러오기
+        if status <= 1:     # 1인 이하
             return render_template('blue.html')
-        elif status == 2:
+        elif status == 2:   # 1인 이하
             return render_template('yellow.html')
-        else:
+        else:               # 3인 이상
             return render_template('red.html')
         
 # 길 통제
 @app.route('/road3')
 def home3():
     while True:
-        status = get_status3()
-        if status <= 2:
+        status = get_status3()          # 중앙 계단에 있는 사람의 수에 따라 각각 다른 HTML파일 불러오기
+        if status <= 2:     # 2인 이하
             return render_template('blue.html')
-        elif status == 3:
+        elif status == 3:   # 3인
             return render_template('yellow3.html')
-        elif status == 4:
+        elif status == 4:   # 4인
             return render_template('yellow4.html')
-        else:
+        else:               # 5인 이상
             return render_template('red5.html')
 
 
--- capture.py
+++ capture.py
@@ -1,37 +1,37 @@
-from ultralytics import solutions, YOLO
+from ultralytics import solutions, YOLO 
 import cv2
-
-status1 = 0
-status2 = 0
 
 # 비디오 스트리밍 함수 정의
 def generate_frame():
-    # cap = cv2.VideoCapture("rtsp://yolo:yolo@192.168.16.92:8080/h264_ulaw.sdp")
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture("rtsp://yolo:yolo@192.168.16.92:8080/h264_ulaw.sdp")       # rtsp 카메라
+    cap = cv2.VideoCapture(0)       # 연결된 기본 카메라
     
     # 특정 좌표 설정
     region_points = {
-        "Region#01": [(0,0), (600,0), (600, 1080), (0, 1080)],
-        "Region#02": [(1250,0), (1920,0), (1920, 1080), (1250, 1080)],
-        "Region#03": [(600,0), (1250,0), (1250, 1080), (600, 1080)]
+        "Region#01": [(0,0), (600,0), (600, 1080), (0, 1080)],          # 좌측 계단 영역
+        "Region#02": [(1250,0), (1920,0), (1920, 1080), (1250, 1080)],  # 우측 계단 영역
+        "Region#03": [(600,0), (1250,0), (1250, 1080), (600, 1080)]     # 중앙 계단 영역
     }
 
     # 구역 설정
     region = solutions.RegionCounter(
         show=True,
         region=region_points,
-       # model='model_train_1/trained_yolo11n.pt'                                # 맥용 경로
-        model = 'Pyeongtaek_AI_YOLO_Team/model_train_1/trained_yolo11n.pt'     # 윈도우용 경로
+        model='model_train_1/trained_yolo11n.pt'                                # 맥용 경로
+        # model = 'Pyeongtaek_AI_YOLO_Team/model_train_1/trained_yolo11n.pt'     # 윈도우용 경로
     )
     
     
     while True:
         success, im0 = cap.read()
-        if not success:
+        if not success:             # 카메라 오류
             print("프레임 확인")
             break
         
-        region_results, region_counts, label_names = region.count(im0)
+        region_results, region_counts = region.count(im0)      # region_counter.py의 코드를 count.txt의 텍스트로 변환 시 사용 가능
+        '''
+        region_count는 딕셔너리 타입으로 각 구역에서 탐지된 객채의 수 추출
+        '''
         
         # 탐지된 객체의 수 추출
         global status1, status2, status3
@@ -51,10 +51,10 @@ def generate_frame():
         
     cap.release()
     
-def get_status1():
+def get_status1():      # 좌측 계단의 사람 수 출력 함수
     return status1
-def get_status2():
+def get_status2():      # 우측 계단의 사람 수 출력 함수
     return status2
-def get_status3():
+def get_status3():      # 충앙 계단의 사람 수 출력 함수
     return status3
 
